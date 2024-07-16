2d358a8a9ebb71f21f165d2aeb6a92c909f781a3
Brenda Moura <brendamoura.bcdm@gmail.com>
2024-07-15 20:31:58 -0400

    Added endpoint to get session information

--- backend/src/resources/auth/auth.router.ts
+++ backend/src/resources/auth/auth.router.ts
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/logout", isAuth, authController.logout);
+router.post("/logged", authController.logged);
 
 export default router;
