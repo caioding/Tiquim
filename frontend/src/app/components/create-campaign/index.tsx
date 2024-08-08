1a15d6925634f3713ed00addf75432c24bbab2d9
Brenda Moura <brendamoura.bcdm@gmail.com>
2024-08-08 17:31:28 -0400

    Fixed modal scroll on create and edit campaign

--- frontend/src/app/components/create-campaign/index.tsx
+++ frontend/src/app/components/create-campaign/index.tsx
@@ -2,13 +2,11 @@
 import {
   Box,
   Button,
-  Card,
-  CardContent,
-  CssBaseline,
+  Dialog,
+  DialogContent,
   Grid,
   IconButton,
   InputLabel,
-  Modal,
   TextField,
   Typography,
 } from "@mui/material";
@@ -95,220 +93,197 @@ export default function CreateCampaignModal({ open, handleClose }: CreateCampaig
   };
 
   return (
-    <Modal open={open} onClose={handleClose} sx={{ overflow: "scroll" }}>
-      <Box maxWidth="md" sx={{ m: "auto" }}>
-        <CssBaseline />
+    <Dialog open={open} onClose={handleClose} scroll="body" maxWidth="md">
+      <DialogContent sx={{ p: 6, backgroundColor: "#f8fafa" }}>
+        <Grid container justifyContent="space-between" alignItems="center" spacing={2}>
+          <Grid item>
+            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+              Criar Campanha
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              sx={{
+                textTransform: "none",
+                backgroundColor: "#32a852",
+                color: "white",
+                "&:hover": { backgroundColor: "#008000" },
+              }}
+              onClick={handleSubmit(handleFormSubmit)}
+            >
+              Salvar
+            </Button>
+          </Grid>
+        </Grid>
         <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            marginBottom: "50px",
-          }}
+          component="form"
+          onSubmit={handleSubmit(handleFormSubmit)}
+          encType="multipart/form-data"
+          sx={{ mt: 3, width: "100%" }}
         >
-          <Card
-            variant="elevation"
-            sx={{
-              minWidth: 275,
-              backgroundColor: "#f8fafa",
-              border: "20px solid #f8fafa",
-              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-            }}
-          >
-            <CardContent>
-              <Grid container justifyContent="space-between" alignItems="center" spacing={2}>
-                <Grid item>
-                  <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-                    Criar Campanha
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Button
-                    variant="contained"
-                    sx={{
-                      textTransform: "none",
-                      backgroundColor: "#32a852",
-                      color: "white",
-                      "&:hover": { backgroundColor: "#008000" },
-                    }}
-                    onClick={handleSubmit(handleFormSubmit)}
-                  >
-                    Salvar
-                  </Button>
-                </Grid>
-              </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <InputLabel htmlFor="title" sx={{ color: "black" }}>
+                Título
+              </InputLabel>
+              <TextField
+                required
+                fullWidth
+                id="title"
+                autoComplete="title"
+                autoFocus
+                variant="outlined"
+                margin="normal"
+                sx={{ backgroundColor: "white" }}
+                inputProps={{ maxLength: 50 }}
+                {...register("title", { required: true })}
+                value={campaignInfo?.title}
+                onChange={(e) => setCampaignInfo({ ...campaignInfo, title: e.target.value })}
+              />
+              {errors.title?.type === "required" && (
+                <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
+              )}
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel htmlFor="goal" sx={{ color: "black" }}>
+                Meta
+              </InputLabel>
+              <FormattedInputs
+                campaignInfo={campaignInfo}
+                setCampaignInfo={setCampaignInfo}
+                register={register}
+                handleSubmit={handleSubmit}
+                errors={errors}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel htmlFor="category" sx={{ color: "black" }}>
+                Categorias
+              </InputLabel>
+              <Grouped
+                campaignInfo={campaignInfo}
+                setCampaignInfo={setCampaignInfo}
+                register={register}
+                handleSubmit={handleSubmit} //n eh necessario
+                errors={errors}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel htmlFor="deadline" sx={{ color: "black" }}>
+                Prazo
+              </InputLabel>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  value={campaignInfo.deadline}
+                  onChange={(date) => setCampaignInfo({ ...campaignInfo, deadline: date! })}
+                  slotProps={{ textField: { variant: "outlined" } }}
+                  format="dd-MM-yyyy"
+                  sx={{ backgroundColor: "white", width: "100%" }}
+                />
+              </LocalizationProvider>
+              {errors.deadline?.type === "required" && (
+                <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
+              )}
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel htmlFor="preview" sx={{ color: "black" }}>
+                Resumo
+              </InputLabel>
+              <TextField
+                required
+                fullWidth
+                id="preview"
+                autoComplete="preview"
+                autoFocus
+                variant="outlined"
+                margin="normal"
+                sx={{ backgroundColor: "white" }}
+                inputProps={{ maxLength: 120 }}
+                {...register("preview", { required: true })}
+                value={campaignInfo.preview}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCampaignInfo({ ...campaignInfo, preview: e.target.value })
+                }
+              />
+              {errors.preview?.type === "required" && (
+                <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
+              )}
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel htmlFor="description" sx={{ color: "black" }}>
+                Descrição
+              </InputLabel>
+              <TextField
+                required
+                fullWidth
+                id="description"
+                multiline
+                rows={4}
+                autoComplete="description"
+                variant="outlined"
+                margin="normal"
+                sx={{ backgroundColor: "white" }}
+                inputProps={{ maxLength: 1000 }}
+                {...register("description", { required: true })}
+                value={campaignInfo.description}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCampaignInfo({ ...campaignInfo, description: e.target.value })
+                }
+              />
+              {errors.description?.type === "required" && (
+                <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
+              )}
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <Box
-                component="form"
-                onSubmit={handleSubmit(handleFormSubmit)}
-                encType="multipart/form-data"
-                sx={{ mt: 3, width: "100%" }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
               >
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <InputLabel htmlFor="title" sx={{ color: "black" }}>
-                      Título
-                    </InputLabel>
-                    <TextField
-                      required
-                      fullWidth
-                      id="title"
-                      autoComplete="title"
-                      autoFocus
-                      variant="outlined"
-                      margin="normal"
-                      sx={{ backgroundColor: "white" }}
-                      inputProps={{ maxLength: 50 }}
-                      {...register("title", { required: true })}
-                      value={campaignInfo?.title}
-                      onChange={(e) => setCampaignInfo({ ...campaignInfo, title: e.target.value })}
-                    />
-                    {errors.title?.type === "required" && (
-                      <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
-                    )}
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <InputLabel htmlFor="goal" sx={{ color: "black" }}>
-                      Meta
-                    </InputLabel>
-                    <FormattedInputs
-                      campaignInfo={campaignInfo}
-                      setCampaignInfo={setCampaignInfo}
-                      register={register}
-                      handleSubmit={handleSubmit}
-                      errors={errors}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <InputLabel htmlFor="category" sx={{ color: "black" }}>
-                      Categorias
-                    </InputLabel>
-                    <Grouped
-                      campaignInfo={campaignInfo}
-                      setCampaignInfo={setCampaignInfo}
-                      register={register}
-                      handleSubmit={handleSubmit} //n eh necessario
-                      errors={errors}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <InputLabel htmlFor="deadline" sx={{ color: "black" }}>
-                      Prazo
-                    </InputLabel>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                      <DatePicker
-                        value={campaignInfo.deadline}
-                        onChange={(date) => setCampaignInfo({ ...campaignInfo, deadline: date! })}
-                        slotProps={{ textField: { variant: "outlined" } }}
-                        format="dd-MM-yyyy"
-                        sx={{ backgroundColor: "white", width: "100%" }}
-                      />
-                    </LocalizationProvider>
-                    {errors.deadline?.type === "required" && (
-                      <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
-                    )}
-                  </Grid>
-                  <Grid item xs={12}>
-                    <InputLabel htmlFor="preview" sx={{ color: "black" }}>
-                      Resumo
-                    </InputLabel>
-                    <TextField
-                      required
-                      fullWidth
-                      id="preview"
-                      autoComplete="preview"
-                      autoFocus
-                      variant="outlined"
-                      margin="normal"
-                      sx={{ backgroundColor: "white" }}
-                      inputProps={{ maxLength: 120 }}
-                      {...register("preview", { required: true })}
-                      value={campaignInfo.preview}
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setCampaignInfo({ ...campaignInfo, preview: e.target.value })
-                      }
-                    />
-                    {errors.preview?.type === "required" && (
-                      <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
-                    )}
-                  </Grid>
-                  <Grid item xs={12}>
-                    <InputLabel htmlFor="description" sx={{ color: "black" }}>
-                      Descrição
-                    </InputLabel>
-                    <TextField
-                      required
-                      fullWidth
-                      id="description"
-                      multiline
-                      rows={4}
-                      autoComplete="description"
-                      variant="outlined"
-                      margin="normal"
-                      sx={{ backgroundColor: "white" }}
-                      inputProps={{ maxLength: 1000 }}
-                      {...register("description", { required: true })}
-                      value={campaignInfo.description}
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setCampaignInfo({ ...campaignInfo, description: e.target.value })
-                      }
-                    />
-                    {errors.description?.type === "required" && (
-                      <Box sx={{ color: "error.main" }}>Esse campo é obrigatório</Box>
-                    )}
-                  </Grid>
-                  <Grid
-                    item
-                    xs={12}
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "center",
-                        alignItems: "center",
-                      }}
-                    >
-                      <Box
-                        component="img"
-                        sx={{
-                          height: 200,
-                          width: 300,
-                          display: selectedFile ? "block" : "none",
-                        }}
-                        src={selectedFile ? URL.createObjectURL(selectedFile) : ""}
-                      />
-                      <InputFileUpload onFileChange={handleFileChange} />
-                      <IconButton
-                        aria-label="delete"
-                        color="success"
-                        sx={{
-                          display: selectedFile ? "block" : "none",
-                        }}
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          setSelectedFile(null);
-                        }}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </Box>
-                    {selectedFile === null && (
-                      <Box sx={{ color: "error.main", mt: "8px" }}>Esse campo é obrigatório</Box>
-                    )}
-                  </Grid>
-                </Grid>
+                <Box
+                  component="img"
+                  sx={{
+                    height: 200,
+                    width: 300,
+                    display: selectedFile ? "block" : "none",
+                  }}
+                  src={selectedFile ? URL.createObjectURL(selectedFile) : ""}
+                />
+                <InputFileUpload onFileChange={handleFileChange} />
+                <IconButton
+                  aria-label="delete"
+                  color="success"
+                  sx={{
+                    display: selectedFile ? "block" : "none",
+                  }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setSelectedFile(null);
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
               </Box>
-            </CardContent>
-          </Card>
+              {selectedFile === null && (
+                <Box sx={{ color: "error.main", mt: "8px" }}>Esse campo é obrigatório</Box>
+              )}
+            </Grid>
+          </Grid>
         </Box>
-      </Box>
-    </Modal>
+      </DialogContent>
+    </Dialog>
   );
 }
